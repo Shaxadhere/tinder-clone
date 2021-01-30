0518f5a6e4914d706ea8e437ac54e1b490cb1eb6
Shaxadhere <Shaxad.here@gmail.com>
2021-01-30 15:40:58 +0500

    express and mongoose

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -2,11 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 //AppConfig
+const app = express();
+const port = process.env.PORT || 8001
 
 //Middleware
 
 //DB Config
 
 //API Endpoints
+app.get('/', (req, res) => res.status(200).send("Hello World"));
 
 //Listeners
+app.listen(port, () => console.log(`listening on localhost: ${port}`))
